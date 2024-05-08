93aa84a6cfb6cf4a308ded4af3135f8da792d9d8
tengfeisubat <139120787+tengfeisubat@users.noreply.github.com>
2024-05-08 15:38:51 +0800

    Update print_cuda.py

--- print_cuda.py
+++ print_cuda.py
@@ -4,10 +4,10 @@ flag = torch.cuda.is_available()
 if flag:
     print("CUDA is available")
 else:
-    print("CUDA not available!")
+    print("CUDA NOT available")
 
 ngpu= 1
 # Decide which device we want to run on
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
-print("DEVICE：",device)
+print("The driver is：",device)
 print("GPU： ",torch.cuda.get_device_name(0))
