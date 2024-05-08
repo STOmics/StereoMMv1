ec21bd2981eb2fa05d9af6fdb5a86abb0e5af399
tengfeisubat <139120787+tengfeisubat@users.noreply.github.com>
2024-05-08 15:39:23 +0800

    Update process_img.py

--- process_img.py
+++ process_img.py
@@ -24,6 +24,7 @@ import random
 import warnings
 import sys
 from torch.utils.data import Dataset
+from utils import find_res_binary,plot_spatial
 warnings.filterwarnings('ignore')
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # device object
@@ -40,7 +41,7 @@ usage = '''
 
 Version %s  by Luo Bingying  %s
 
-Usage: %s -i <gem_file> -o <output_dir> [...]
+Usage: %s -a <adata_file> -i <gem_file> -o <output_dir> [...]
 
 ''' % (PROG_VERSION, PROG_DATE, os.path.basename(sys.argv[0]))
 
@@ -123,20 +124,21 @@ def save_data(adata,save_path,label_txt_file,adata_file):
     adata.write(os.path.join(save_path,adata_file))
     
 class MyDataset(Dataset): 
-    def __init__(self, txt_path, transform=None, target_transform=None): 
+    def __init__(self, txt_path, transform=None, target_transform=None):
         fh = open(txt_path, 'r') 
         imgs = []  
         for line in fh:
             line = line.rstrip() 
             words = line.split() 
-            imgs.append((words[0], int(words[1]))) 
+            imgs.append((words[0], int(words[1])))
 
-        self.imgs = imgs        
+        self.imgs = imgs       
         self.transform = transform
         self.target_transform = target_transform
 
     def __getitem__(self, index):
-        fn, label = self.imgs[index] 
+        fn, label = self.imgs[index]
+        # img = Image.open(fn).convert('RGB')    
         img = Image.open(fn) 
 
         if self.transform is not None:
@@ -150,13 +152,18 @@ class MyDataset(Dataset):
     
 def load_model():
     model_com = models.resnet50(pretrained=False)
-    model_com.load_state_dict(torch.load("./torch_pths/resnet50-19c8e357.pth"))
+    current_path = os.getcwd()
+    if 'StereoMMv1' in current_path:
+        pth_path = os.path.join(current_path, "torch_pths/resnet50-19c8e357.pth")
+    else:
+        pth_path = os.path.join(current_path, "StereoMMv1/torch_pths/resnet50-19c8e357.pth")
+    model_com.load_state_dict(torch.load(pth_path))
     num_features = model_com.fc.in_features
 
     ### strip the last layer
     feature_extractor = torch.nn.Sequential(*list(model_com.children())[:-1])
     # Get the feature extractor up to layer4
-    #feature_extractor_layer4 = torch.nn.Sequential(*list(model.children())[:-2])
+    # feature_extractor_layer4 = torch.nn.Sequential(*list(model.children())[:-2])
 
     model_com.to(device)
     model_com.eval()
@@ -264,7 +271,7 @@ def choose_res(adata,res_range,method = 'num_cluster',cluster_num = 10,criterion
                 return res
 
         if not found:
-            print("No suitable resolution found!")      
+            print("No suitable resolution found")      
 
 def definite_res(adata,res,save_path,plot_file='choose_res_spatial_plot.png',title=None):
     sc.tl.leiden(adata, resolution=res)
@@ -290,16 +297,18 @@ def main():
     ArgParser.add_argument("-o", "--output", action="store", dest="output", required=True, type=str, help="output folder")
     ArgParser.add_argument("-b", "--bin_size", action="store", dest="bin_size", required=True, type=int, help="bin size of adata")
     ArgParser.add_argument("-c", "--crop_size", action="store", dest="crop_size", required=True, type=int, help="crop size of image")
+    ArgParser.add_argument("-n", "--num_cluster", action="store", dest="num_cluster", required=False, default=10, type=int, help="number of cluster")
     ArgParser.add_argument("-g", "--slide_gem",  action="store_true", dest="slide_gem", default=False, help="gem file for whole slide")
-    ArgParser.add_argument("-r", "--res_range", dest="res_range", required=False, default=np.around(np.arange(0.3,0.9,0.02), 3), type=float,help="resolution range for clustering")
+    # ArgParser.add_argument("-r", "--res_range", dest="res_range", required=False, default=np.around(np.arange(0.3,0.9,0.02), 3), type=float,help="resolution range for clustering")
     (para, args) = ArgParser.parse_known_args()
     print(para,'\n')
     #print(type(para.bin_size))
     
     num_gpus = torch.cuda.device_count()
     print("number of GPUs：", num_gpus)
-    current_gpu = torch.cuda.current_device()
-    print("The GPU index currently in use：", current_gpu)
+    if num_gpus!=0:
+        current_gpu = torch.cuda.current_device()
+        print("The GPU index currently in use：", current_gpu)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     if torch.cuda.is_available():
@@ -335,6 +344,7 @@ def main():
         mkdir(tile_path)
         crop_size = int(para.crop_size)  
         rna_adata = image_crop(image_added,rna_adata,save_path=tile_path,crop_size=crop_size,verbose=False)
+        # rna_adata = image_crop(image_raw,rna_adata,save_path=tile_path,crop_size=crop_size,verbose=False)
         plot_spatial_with_img(rna_adata,para.output,file='rna_spatial_with_img.png')
 
         save_data(rna_adata,para.output,label_txt_file=label_txt_file,adata_file=adata_file)
@@ -345,7 +355,6 @@ def main():
         feat_outputs = pd.read_pickle(os.path.join(para.output,'img_feat.pkl'))
     else:
         feature_extractor,model_com = load_model()
-        # 并行
         # if torch.cuda.device_count() > 1:
         #     print("Turn on parallelism: use multiple GPUs for training")
         #     feature_extractor = torch.nn.DataParallel(feature_extractor)
@@ -357,9 +366,13 @@ def main():
         print('=======================finish generate image feature=============================')
     
     img_adata = generate_adata(feat_outputs.values,spatial=rna_adata.obsm['spatial'])
-    res_range = para.res_range
-    res = choose_res(img_adata,res_range,method = 'num_cluster',cluster_num = 10,criterion = 'CH_score')
-    img_adata = definite_res(img_adata,res,para.output,plot_file='image_spatial_plot.png',title='image_feat_leiden')
+    print(f"Clustering H&E image feature at number of clusters: {para.num_cluster}")
+    resolution, img_adata = find_res_binary(img_adata, resolution_min=0.1, resolution_max=1.2, num_clusters=para.num_cluster,key_added='cluster')
+    print(f"Final Resolution: {resolution}")
+    plot_spatial(img_adata,para.output,title ='H&E morphology featuer domain',group='cluster')
+    # res_range = para.res_range
+    # res = choose_res(img_adata,res_range,method = 'num_cluster',cluster_num = 10,criterion = 'CH_score')
+    # img_adata = definite_res(img_adata,res,para.output,plot_file='image_spatial_plot.png',title='image_feat_leiden')
     # img_adata = find_clusters(img_adata,cluster_range=12,verbose=True)
     # img_adata = definite_res(img_adata, img_adata.uns['best_resolution'],para.output,plot_file='choose_res_spatial_plot.png',title=None)
     img_adata.write(os.path.join(para.output,'img_adata.h5ad'))
